1f22b3e5628cb8dd8725130ac9b282bb6c99721f
bi.j <bi.j@burgeon.cn>
2020-08-16 17:02:20 +0800

    'lll'

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="content"></div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  data() {
+    return {
+      websock: null
+    };
+  },
+  created() {
+    this.initWebSocket();
+  },
+  destroyed() {
+    this.websock.close(); //离开路由之后断开websocket连接
+  },
+  methods: {
+    initWebSocket() {
+      //初始化weosocket
+      const wsuri = "ws://106.12.192.100:8577/websocket";
+      this.websock = new WebSocket(wsuri);
+      this.websock.onmessage = this.websocketonmessage;
+      this.websock.onopen = this.websocketonopen;
+      this.websock.onerror = this.websocketonerror;
+      this.websock.onclose = this.websocketclose;
+    },
+    websocketonopen() {
+      //连接建立之后执行send方法发送数据
+      let actions = { test: "12345" };
+      console.log("ddddd");
+      this.websocketsend(JSON.stringify(actions));
+    },
+    websocketonerror() {
+      //连接建立失败重连
+      this.initWebSocket();
+    },
+    websocketonmessage(e) {
+      //数据接收
+      const redata = JSON.parse(e.data);
+    },
+    websocketsend(Data) {
+      //数据发送
+      this.websock.send(Data);
+    },
+    websocketclose(e) {
+      //关闭
+      console.log("断开连接", e);
+    }
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.content {
+  width: 800px;
+  background-color: #ccc;
+  margin: 0 auto;
+  height: 600px;
+}
+</style>
---
+++ src/components/login.vue
@@ -0,0 +1,76 @@
+<template>
+  <div class="content">
+    <el-form
+      :model="ruleForm"
+      :rules="rules"
+      ref="ruleForm"
+      label-width="100px"
+      class="demo-ruleForm"
+    >
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="登录" name="first"></el-tab-pane>
+        <el-tab-pane label="注册" name="two"></el-tab-pane>
+      </el-tabs>
+      <el-form-item v-if="activeName=='first'" label="账户" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item v-if="activeName=='first'" label="密码" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item v-if="activeName=='two'" label="手机号" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item v-if="activeName=='two'" label="验证码" prop="name">
+        <el-input v-model="ruleForm.name"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button v-if="activeName=='first'" type="primary" @click="submitForm('ruleForm')">登录</el-button>
+        <el-button v-if="activeName=='two'" type="primary" @click="submitForm('ruleForm')">注册</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "login",
+  data() {
+    return {
+      activeName: "first",
+      ruleForm: {
+        name: ""
+      },
+      rules: {
+        name: [
+          { required: true, message: " ", trigger: "blur" },
+          { min: 3, max: 5, message: " ", trigger: "blur" }
+        ]
+      }
+    };
+  },
+  methods: {
+    submitForm(formName) {
+      this.$refs[formName].validate(valid => {
+        if (valid) {
+          console.log("dd");
+        } else {
+          console.log("error submit!!");
+          return false;
+        }
+      });
+    },
+    handleClick(tab, event) {
+      console.log(tab, event);
+    }
+  }
+};
+</script>
+
+<style scoped>
+.content {
+  width: 800px;
+  background-color: #ccc;
+  margin: 0 auto;
+  height: 600px;
+}
+</style>
